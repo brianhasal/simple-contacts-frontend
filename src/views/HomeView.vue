9d9b9991a3be4a367d6e4d2cd199589338df9c25
Brian Hasal <brian.hasal@gmail.com>
2022-05-31 04:50:36 -0700

    Initial Commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
+<script>
+import axios from 'axios';
+export default {
+  data: function () {
+    return {
+      contacts: []
+    };
+  },
+  created: function () {
+    this.indexContacts();
+  },
+  methods: {
+    indexContacts: function() {
+      axios.get("/contacts").then((response) => {
+        console.log("contacts index", response);
+        this.contacts = response.data;
+      })
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Contacts</h1>
+    <div v-for="contact in contacts" v-bind:key="contact.id">
+      <div class="image">
+        <img v-bind:src="contact.image" v-bind:alt="contact.name">       
+      </div>
+      <h2>{{ contact.first_name }} {{ contact.last_name }}</h2>
+      <h3>{{contact.email}} * {{contact.phone_number}}</h3>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+<style>
+  img {
+      max-width: 50%;
   }
-}
-</script>
+</style>
